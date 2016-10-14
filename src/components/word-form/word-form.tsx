cc6bfb9c83f2a2415f2948df51c059f399a4f355
akos@pingdom.com <akos@pingdom.com>
2016-10-14 21:16:47 +0200

    adding word check

--- src/components/word-form/word-form.tsx
+++ src/components/word-form/word-form.tsx
@@ -10,20 +10,30 @@ export class WordForm extends React.Component<IWord, IWordState> {
     }
     private onInputChange(event: any) {
         console.log(event.target.value);
-        //this.state = {inputValue: event.target.value};
         this.setState({inputValue: event.target.value});
+    }
+    private submit(event: any) {
+        event.preventDefault();
+        console.log("formSubmit", this.state.inputValue);
+        if (this.state.inputValue === this.props.word.content[this.props.appState.to]) {
+            console.log("ok");
+        } else {
+            console.log("ajjjajjaj");
+        }
+        return false;
     }
     render() {
         var to = this.props.appState.to;
         var from = this.props.appState.from;
-        var baseWord: sting = this.props.word.content[from];
+        var baseWord: string = this.props.word.content[from];
         var learnWord: string = this.props.word.content[to];
         return (
-        	<div>
+        	<form onSubmit={this.submit.bind(this)}>
         		<h1>This is a word</h1>
                 <p>{baseWord}</p>
                 <input type="text" value={this.state.inputValue} onChange={this.onInputChange.bind(this)}/>
-        	</div>
+                <button>check</button>
+        	</form>
         );
     }
 }
